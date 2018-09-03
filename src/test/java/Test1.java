5d87e040981245cf377f533b0c5f157f4777e052
mdrahman <mdrahman.tech@gmail.com>
2018-09-02 21:26:56 -0400

    master Commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by mdrahman on 9/2/18.
+ * Spring 2018
+ */
+public class Test1 {
+
+    @Test
+    public void run(){
+        System.out.println("New Run");
+    }
+
+
+}
